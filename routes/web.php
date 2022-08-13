81106bbf1697cbbd1861e04762168b01b4ebbbc8
Stephen Uweh <stephen.uweh@gmail.com>
2022-08-13 14:54:41 +0100

    Edit

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test/api', function(){
+    return "API";
+});
+
 Route::get('/pending', function(){
     return view('pending');
 });
