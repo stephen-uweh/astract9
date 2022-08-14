aea2f96ce0deeb202a79aac2886c109c4282e8fd
Stephen Uweh <stephen.uweh@gmail.com>
2022-08-14 17:18:48 +0100

    Admin Auth Update 17

--- app/Http/Controllers/UserDashboardController.php
+++ app/Http/Controllers/UserDashboardController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Messages;
 use Illuminate\Http\Request;
 
 class UserDashboardController extends Controller
 {
     //
     public function user_messages(){
-        $messages = auth()->user()->messages;
+        $user = auth()->user();
+        $messages = Messages::orderBy('created_at', 'desc')->where('user_id', $user->id)->get();
         return view('messages', compact($messages));
     }
 
@@ -17,12 +19,19 @@ class UserDashboardController extends Controller
     }
 
     public function send_message(Request $request){
+        $user = auth()->user();
         $this->validate($request, [
             'subject' => 'required',
             'message' => 'required'
         ]);
+        
+        $data = [
+            'user_id' => $user->id,
+            'subject' => $request->subject,
+            'message' => $request->message
+        ];
 
-        auth()->user()->messages->create($request->all());
+        Messages::create($data);
         
         return redirect('/user/dashboard');
     }
